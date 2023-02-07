8f8f5ee7026484ea6b7f99ea310a02b417721a54
swalehaarab009 <118881700+swalehaarab009@users.noreply.github.com>
2023-02-07 17:40:37 +0530

    Update calc.py

--- calc/calc.py
+++ calc/calc.py
@@ -9,9 +9,9 @@ def subtract(x, y):
     return x - y
 
 
-def multiply(x, y):
-    """Multiply Function"""
-    return x * y
+# def multiply(x, y):
+#     """Multiply Function"""
+#     return x * y
 
 
 def divide(x, y):
